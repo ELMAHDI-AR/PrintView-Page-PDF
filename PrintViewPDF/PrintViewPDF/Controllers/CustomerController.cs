beb7f0c65bff51804a4b85bf8331154acb79ae9f
ELMAHDI-AR <el-mahdi.archane@hotmail.com>
2018-01-30 20:56:16 -0800

    Commit1

---
+++ PrintViewPDF/PrintViewPDF/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using Rotativa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrintViewPDF.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //DbPrintPDFEntities
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using (DbPrintPDFEntities db = new DbPrintPDFEntities())
+            {
+                var customerList = db.Customers.ToList();
+
+                return View(customerList);
+            }
+        }
+
+        public ActionResult PrintViewToPdf()
+        {
+            var report = new ActionAsPdf("Index");
+            return report;
+        }
+
+
+        public ActionResult DetailCustomer(int idCustomer)
+        {
+            using (DbPrintPDFEntities db = new DbPrintPDFEntities())
+            {
+                Customer customer = db.Customers.FirstOrDefault(c => c.CustomerId == idCustomer);
+                return PartialView("~/Views/Shared/DetailCustomer.cshtml", customer);
+            }
+        }
+
+        public ActionResult PrintPartialViewToPdf(int id)
+        {
+            using (DbPrintPDFEntities db = new DbPrintPDFEntities())
+            {
+                Customer customer = db.Customers.FirstOrDefault(c => c.CustomerId == id);
+
+                var report = new PartialViewAsPdf("~/Views/Shared/DetailCustomer.cshtml", customer);
+                return report;
+            }
+           
+        }
+    }
+}
